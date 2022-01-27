bd4fa080f72416bc028a791268c42fd341cdd86c
Iván L. Murillo <diskman199@gmail.com>
2022-01-26 19:13:03 -0600

    Manejo de errores y guardar datos raw

--- sniffer.py
+++ sniffer.py
@@ -1,3 +1,5 @@
+import os
+
 tipos = {
     ('08', '00') : 'IPv4',
     ('08', '06') : 'ARP',
@@ -9,21 +11,31 @@ class Sniffer:
     def __init__(self, filename):
         print('*'*100)
         self.filename = filename
-        self.bytes = []
 
-        with open(self.filename, 'rb') as file:
-            self.bytes = []
-            byte = file.read(1) #Leer 1 byte
+        try:
+            with open(self.filename, 'rb') as file:
+                self.bytes = []
+                self.raw_bytes = []
+                byte = file.read(1) #Leer 1 byte
 
-            while byte:         #Mientras haya mas contenido
-                hex_digit = byte.hex().upper()  #Representación del string
-                self.bytes.append(hex_digit)                
-                byte = file.read(1)
-            
-            print('Contenido: ' + ' '.join(self.bytes))
-    
+                while byte:         #Mientras haya mas contenido
+                    hex_digit = byte.hex().upper()  #Representación del string
+                    
+                    self.bytes.append(hex_digit)                
+                    self.raw_bytes.append(byte)
+                    
+                    byte = file.read(1)
+                
+                print('Contenido: ' + ' '.join(self.bytes))
+        except Exception as e:
+            self.bytes = None
+            print('Error al leer el archivo')
+            return
 
     def handle(self):
+        if not self.is_valid():
+            return
+        
         self.destino = ':'.join(self.bytes[0:6])
         self.origen =  ':'.join(self.bytes[6:12])
         self.tipo = tuple(self.bytes[12:14])
@@ -54,3 +66,6 @@ class Sniffer:
         else:
             
             print('No soportado aun')
+
+    def is_valid(self):
+        return self.bytes is not None
